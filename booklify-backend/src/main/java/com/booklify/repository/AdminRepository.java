dd43c9deb0aa361a095770baeef94fee708971b8
Ranelan <raneyclassic@gmail.com>
2025-07-27 23:47:45 +0200

    Regular user and Admin Repository added

--- booklify-backend/src/main/java/com/booklify/repository/AdminRepository.java
+++ booklify-backend/src/main/java/com/booklify/repository/AdminRepository.java
@@ -1,4 +1,12 @@
 package com.booklify.repository;
 
-public class AdminRepository {
+import com.booklify.domain.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AdminRepository extends JpaRepository <Admin, Long>{
+
+    List<Admin> findByEmail(String email);
+
 }
