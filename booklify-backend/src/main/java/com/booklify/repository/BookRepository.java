2a831f822621c935ae0c7564dc7e74c7333f00aa
Pelisa98 <217189113@mycput.ac.za>
2025-08-11 16:18:52 +0200

    book repository interface

--- booklify-backend/src/main/java/com/booklify/repository/BookRepository.java
+++ booklify-backend/src/main/java/com/booklify/repository/BookRepository.java
@@ -1,4 +1,28 @@
 package com.booklify.repository;
 
-public interface BookRepository {
+import com.booklify.domain.Book;
+import com.booklify.domain.enums.BookCondition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByCondition(BookCondition condition);
+
+    List<Book> findByPriceLessThanEqual(Double maxPrice);
+
+//    List<Book> findBySeller_Id(Long sellerId);
+
+    @Query("SELECT b FROM Book b WHERE b.price > :minPrice AND b.condition = :condition")
+    List<Book> findBooksByMinPriceAndCondition(@Param("minPrice") Double minPrice, @Param("condition") BookCondition condition);
 }
+
+
