5f0ebfde9639842e995ff96e90be85cebab8b455
Ranelan <raneyclassic@gmail.com>
2025-08-14 02:15:28 +0200

    Update Tests

--- booklify-backend/src/main/java/com/booklify/repository/OrderRepository.java
+++ booklify-backend/src/main/java/com/booklify/repository/OrderRepository.java
@@ -1,4 +1,17 @@
 package com.booklify.repository;
 
-public class OrderRepository {
+import com.booklify.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByRegularUserId(Long id);
+    List<Order> findByOrderDate(LocalDateTime orderDate);
+    
+    @Query("SELECT o FROM Order o WHERE o.orderDate >= :startOfDay AND o.orderDate < :endOfDay")
+    List<Order> findByOrderDateBetween(@Param("startOfDay") LocalDateTime startOfDay, @Param("endOfDay") LocalDateTime endOfDay);
 }
