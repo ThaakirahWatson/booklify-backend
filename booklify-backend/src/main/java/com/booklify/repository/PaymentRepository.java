b3b2eec19006027041e546f31b3e9500e95ee97c
Ranelan <raneyclassic@gmail.com>
2025-08-20 23:31:07 +0200

    Your message

--- booklify-backend/src/main/java/com/booklify/repository/PaymentRepository.java
+++ booklify-backend/src/main/java/com/booklify/repository/PaymentRepository.java
@@ -1,10 +1,4 @@
 package com.booklify.repository;
 
-import com.booklify.domain.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    // Add custom queries if needed
+public class PaymentRepository {
 }
